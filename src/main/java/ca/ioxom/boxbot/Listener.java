78c6afb81637915e77ed8b1d5c9688328bbb9a14
Admiral <67080008+iiAdmiralFatal@users.noreply.github.com>
2021-02-01 15:04:54 -0600

    did some stuffs

--- src/main/java/ca/ioxom/boxbot/Listener.java
+++ src/main/java/ca/ioxom/boxbot/Listener.java
@@ -18,29 +18,37 @@ public class Listener extends ListenerAdapter {
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         switch (event.getMessage().getContentRaw().toLowerCase()) {
-            case "-box yes" :
-                event.getChannel().sendMessage("Box is here :package: ").queue();
-                break;
             case "-box add" :
-                String addToBox = event.getMessage().getContentRaw();
-                try {
+                event.getChannel().sendMessage("proof that im getting the command").queue();
+                /*try {
+                    String addToBox = event.getMessage().getContentRaw().toLowerCase();
                     FileWriter myWriter = new FileWriter("C:\\Users\\darby\\Documents\\GitHub\\boxbot\\src\\main\\java\\ca\\ioxom\\boxbot\\Files\\boxmoment.txt");
                     myWriter.write("\n" + Arrays.toString(addToBox.split("-box add")));
                     myWriter.close();
+                    event.getChannel().sendMessage("Succesfully added item" + addToBox + "to box").queue();
+                    System.out.println("i tried \n\n\n\n\n\n\n\n\nagain");
 
 
 
                 } catch (IOException e) {
+                    System.out.println("did something wrong i think\n\n\n\n\n\nnotice me senpai");
                     e.printStackTrace();
-                }
+                }*/
+                break;
+            case "-box yes" :
+                event.getChannel().sendMessage("Box is here :package: ").queue();
+                break;
+
+
+
 
 
         }
-        ObjectMapper mapper = new ObjectMapper().setDefaultPrettyPrinter(new DefaultPrettyPrinter());
+       /* ObjectMapper mapper = new ObjectMapper().setDefaultPrettyPrinter(new DefaultPrettyPrinter());
         try {
             mapper.writeValue(new File("box_data.json"), new Box(new CustomUser(event.getAuthor()), event.getAuthor()));
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        } */
     }
 }
