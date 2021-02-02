289d008a134a0b5b2e4d55af0639be959f046107
Ioxom <jackwheppner@gmail.com>
2021-02-01 20:54:56 -0600

    delete garbage

--- src/main/java/ca/ioxom/boxbot/Listener.java
+++ src/main/java/ca/ioxom/boxbot/Listener.java
@@ -1,7 +1,6 @@
 package ca.ioxom.boxbot;
 
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -10,33 +9,13 @@ import java.awt.Color;
 
 //unfinished, being worked on by Thonkman
 public class Listener extends ListenerAdapter {
-    private boolean isCheckingForResponse;
-    private User checkedUser;
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         switch (event.getMessage().getContentRaw().toLowerCase()) {
-            case "-box add" :
-                event.getChannel().sendMessage("proof that im getting the command").queue();
-                /*try {
-                    String addToBox = event.getMessage().getContentRaw().toLowerCase();
-                    FileWriter myWriter = new FileWriter("C:\\Users\\darby\\Documents\\GitHub\\boxbot\\src\\main\\java\\ca\\ioxom\\boxbot\\Files\\boxmoment.txt");
-                    myWriter.write("\n" + Arrays.toString(addToBox.split("-box add")));
-                    myWriter.close();
-                    event.getChannel().sendMessage("Succesfully added item" + addToBox + "to box").queue();
-                    System.out.println("i tried \n\n\n\n\n\n\n\n\nagain");
-
-
-
-                } catch (IOException e) {
-                    System.out.println("did something wrong i think\n\n\n\n\n\nnotice me senpai");
-                    e.printStackTrace();
-                }*/
-                break;
             case "-box yes" :
                 event.getChannel().sendMessage("Box is here :package: ").queue();
                 break;
             case "-box help" :
-                //this is the format I want
                 EmbedBuilder helpEmbed = new EmbedBuilder()
                         .setAuthor("The Box People :tm:")
                         .setColor(new Color(0xfc03df))
