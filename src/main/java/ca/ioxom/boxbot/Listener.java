2c6b78d9c209c193e484030a9616addc855b5d91
Ioxom <jackwheppner@gmail.com>
2021-02-01 18:09:46 -0600

    changes my Thonkman

--- src/main/java/ca/ioxom/boxbot/Listener.java
+++ src/main/java/ca/ioxom/boxbot/Listener.java
@@ -1,9 +1,12 @@
 package ca.ioxom.boxbot;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+
+import java.awt.Color;
 
 public class Listener extends ListenerAdapter {
     private boolean isCheckingForResponse;
@@ -30,6 +33,17 @@ public class Listener extends ListenerAdapter {
                 break;
             case "-box yes" :
                 event.getChannel().sendMessage("Box is here :package: ").queue();
+                break;
+            case "-box help" :
+                //this is the format I want
+                EmbedBuilder helpEmbed = new EmbedBuilder()
+                        .setAuthor("The Box People :tm:")
+                        .setColor(new Color(0xfc03df))
+                        .addField("What in the heck does this bot do?", "This bot is very hot, it stores cool things for you. Like words, words and more words for now.", false)
+                        .addField("Commands", "Prefix is -box \n Commands are the following; yes, add", false)
+                        .addField("The IoxCorp Incorporated", "IoxCorp Incorporated was founded in 04/01/20 by the \"Ioxom Foundation\", and is actively being sponsored by \" The Birch Tree\", It is also funded by \"Thonkman\"\nIf you would like to learn more, please use the command -ioxcorp (nonexistant atm)", false)
+                        .setFooter("Powered by electricity *and ioxcorp:tm:*");
+                event.getChannel().sendMessage(helpEmbed.build()).queue();
                 break;
         }
     }
