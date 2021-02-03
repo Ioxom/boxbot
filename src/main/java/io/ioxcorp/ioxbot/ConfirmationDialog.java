a23dfab15c11e84e91b090086b249d859ae879ab
Ioxom <jackwheppner@gmail.com>
2021-02-03 13:22:06 -0600

    add plan for how confirmation dialog could work

---
+++ src/main/java/io/ioxcorp/ioxbot/ConfirmationDialog.java
@@ -0,0 +1,12 @@
+package io.ioxcorp.ioxbot;
+
+public class ConfirmationDialog {
+    //plan:
+    //make this class use its own dedicated thread so it can properly sleep while awaiting confirmation
+    //use a private boolean object that saves whether we're awaiting confirmation
+    //also save the user id so we know who to accept messages from
+    //use a MessageReceivedEvent that only triggers anything if said boolean variable is true
+    //if we get "yes" or "true" from the user being asked to confirm tell the place asking "true"
+    //if we get "no" or "false" from the user being asked to confirm tell the place asking "false"
+    //we're going to need some sort of await() method that makes the thread sleep until a condition is true
+}
