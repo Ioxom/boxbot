2f4a6d6cba32f0e664c8ab1c311496213921ee5e
Ioxom <jackwheppner@gmail.com>
2021-02-19 15:53:02 -0600

    what is this

--- src/main/java/io/ioxcorp/ioxbox/frame/consoleCrapEdition.java
+++ src/main/java/io/ioxcorp/ioxbox/frame/consoleCrapEdition.java
@@ -2,30 +2,34 @@ package io.ioxcorp.ioxbox.frame;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class consoleCrapEdition extends Application {
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 
     TextField commandTextField = new TextField();
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         BorderPane pane = new BorderPane();
         pane.setPrefSize(300, 300);
         pane.setBottom(commandTextField);
         Scene scene = new Scene(pane);
 
         primaryStage.setScene(scene);
-        primaryStage.setTitle("IoxBot and smt else should go here");
+        primaryStage.setTitle(System.getProperty("javafx.version"));
         primaryStage.show();
 
+        //I'm gnot a gnoblin
+        //I'm gnot a gnome
+        //I'm a g ronk
+        //and you've been
+        //gronked!!
 
-
+        //share this will all your friends to gronk them
     }
 }
