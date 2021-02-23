ce6b6583340d7c7b4c2fa61c276b2a5f32fedddb
Admiral <67080008+iiAdmiralFatal@users.noreply.github.com>
2021-02-22 21:00:55 -0600

    still condensing, shouldve used a speed upgrade

--- src/main/java/io/ioxcorp/ioxbox/frame/ConsoleScrapEdition.java
+++ src/main/java/io/ioxcorp/ioxbox/frame/ConsoleScrapEdition.java
@@ -3,14 +3,21 @@ package io.ioxcorp.ioxbox.frame;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class ConsoleScrapEdition extends Application {
 
     Button btn = new Button("Reload");
+    TextArea output = new TextArea("ecks dee");
+
 
     public static void main(String[] args) {
         launch(args);
@@ -19,6 +26,15 @@ public class ConsoleScrapEdition extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Scrap time");
+
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(10, 10, 10 ,10));
+        gridPane.setVgap(8);
+        gridPane.setHgap(10);
+
+        gridPane.setConstraints(output, 0, 0);
+
+
 
         btn.setOnAction(event -> {
             System.out.println("yesh");
@@ -31,10 +47,12 @@ public class ConsoleScrapEdition extends Application {
         StackPane layout = new StackPane();
         layout.getChildren().add(btn);
 
+
+
         Scene scene = new Scene(layout, 500, 300);
         scene.getStylesheets().add("scrap.css");
-        primaryStage.setScene(scene);
 
+        primaryStage.setScene(scene);
         primaryStage.show();
 
     }
