7bde211ae46d1347371d59f6466439f95fa32578
Ioxom <jackwheppner@gmail.com>
2021-02-23 19:39:22 -0600

    initial frame appearance redo -add buttons -add input console

--- src/main/java/io/ioxcorp/ioxbox/frame/Frame.java
+++ src/main/java/io/ioxcorp/ioxbox/frame/Frame.java
@@ -8,6 +8,7 @@ import io.ioxcorp.ioxbox.frame.logging.LogType;
 import net.dv8tion.jda.api.entities.User;
 
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -30,55 +31,138 @@ import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 public final class Frame {
     private final JTextArea console;
     private final JFrame jFrame;
-    private final JPanel panel;
+    private final JPanel mainPanel;
     private final FileLogger logger;
+    private JTextArea consoleInput;
+    private JButton reloadJDA;
+    private JButton clearConsole;
+    private JButton commandHelp;
 
     public Frame() {
         this.jFrame = new JFrame("ioxbox v " + Main.getVersion());
         this.console = new JTextArea("[init] loading ioxbox");
-        this.panel = new JPanel(new BorderLayout());
+        this.mainPanel = new JPanel(new BorderLayout(0, 0));
         this.logger = new FileLogger();
+        this.consoleInput = new JTextArea();
+        this.reloadJDA = new JButton();
+        this.commandHelp = new JButton();
+        this.clearConsole = new JButton();
     }
 
+//    public void init() {
+//        //configure the console, adding a scroll bar and setting the colour
+//        //we also disallow editing for the console because yeah obvious
+//        final Dimension consoleSize = new Dimension(500, 380);
+//        this.console.setBackground(Color.GRAY);
+//        this.console.setPreferredSize(consoleSize);
+//        this.console.setEditable(false);
+//        this.console.setLineWrap(true);
+//
+//        //create a pane to allow the console to have scrolling
+//        JScrollPane consoleScrollPane = new JScrollPane(this.console);
+//        consoleScrollPane.setPreferredSize(new Dimension(consoleSize.width + 100, consoleSize.height + 100));
+//
+//        JScrollPane pane = new JScrollPane(this.console);
+//        pane.setPreferredSize(consoleSize);
+//
+//        //input console
+////        this.consoleInput = new JTextArea("enter input here");
+////        Dimension consoleInputSize = new Dimension(500, 20);
+////        this.consoleInput.setPreferredSize(consoleInputSize);
+////        this.consoleInput.setEditable(true);
+//
+//////        //buttons
+//////        this.commandHelp = new JButton();
+//////        this.commandHelp.setPreferredSize(new Dimension(50, 100));
+//////        this.commandHelp.addActionListener(e -> this.console.append("\nh"));
+//
+//        //configure the main background panel
+//        this.mainPanel.setPreferredSize(new Dimension(consoleSize.width - 50, consoleSize.height + consoleInputSize.height));
+//        this.mainPanel.add(pane);
+////        this.mainPanel.add(commandHelp, BorderLayout.EAST);
+////        this.mainPanel.add(consoleInput, BorderLayout.SOUTH);
+//        this.jFrame.setContentPane(this.mainPanel);
+//
+//        //set icon
+//        try {
+//            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("images/box.png");
+//            if (inputStream != null) {
+//                Image image = ImageIO.read(inputStream);
+//                this.jFrame.setIconImage(image);
+////                this.log(LogType.INIT, "added icon to frame");
+//            } else {
+//                this.log(LogType.ERR, "failed to add icon to frame; resources may be broken");
+//            }
+//        } catch (IOException e) {
+//            this.log(LogType.FATAL_ERR, "an IOException occurred while reading file \"images/box.png\"");
+//        }
+//
+//        //open the frame
+//        this.jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+//        this.jFrame.setSize(new Dimension(600, 275));
+//        this.jFrame.setVisible(true);
+////        this.log(LogType.INIT, "finished initialising frame");
+//    }
+
     public void init() {
+        this.jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         //set icon
         try {
             InputStream inputStream = getClass().getClassLoader().getResourceAsStream("images/box.png");
             if (inputStream != null) {
                 Image image = ImageIO.read(inputStream);
                 this.jFrame.setIconImage(image);
-                this.log(LogType.INIT, "added icon to FRAME");
+//                this.log(LogType.INIT, "added icon to frame");
             } else {
-                this.log(LogType.ERR, "failed to add icon to FRAME; resources may be broken");
+                this.log(LogType.ERR, "failed to add icon to frame; resources may be broken");
             }
         } catch (IOException e) {
             this.log(LogType.FATAL_ERR, "an IOException occurred while reading file \"images/box.png\"");
         }
-
         //configure the console, adding a scroll bar and setting the colour
-        //we also disallow editing for the console because yeah obvious
-        final Dimension consoleSize = new Dimension(500, 375);
+        final Dimension consoleSize = new Dimension(600, 375);
         this.console.setBackground(Color.GRAY);
         this.console.setSize(consoleSize);
         this.console.setEditable(false);
-
         //create a pane to allow the console to have scrolling
         JScrollPane pane = new JScrollPane(this.console);
         pane.setPreferredSize(consoleSize);
-
         //configure the main background panel
-        this.panel.setPreferredSize(consoleSize);
-        this.panel.add(pane);
-        this.jFrame.setContentPane(this.panel);
-        this.log(LogType.INIT, "added console to FRAME");
+        this.mainPanel.setPreferredSize(consoleSize);
+        this.mainPanel.add(pane);
+        this.jFrame.setContentPane(this.mainPanel);
 
-        //open the FRAME
-        this.jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //yes
+        //input console
+        this.consoleInput = new JTextArea("enter input here");
+        Dimension consoleInputSize = new Dimension(600, 20);
+        this.consoleInput.setPreferredSize(consoleInputSize);
+        this.consoleInput.setEditable(true);
+
+        this.mainPanel.add(this.consoleInput, BorderLayout.SOUTH);
+
+        //add **buttons**
+        //TODO: icons
+        this.reloadJDA = new JButton();
+        this.commandHelp = new JButton();
+        this.clearConsole = new JButton();
+        this.reloadJDA.setPreferredSize(new Dimension(50, 50));
+        this.reloadJDA.setBackground(Color.DARK_GRAY);
+        this.commandHelp.setPreferredSize(new Dimension(50, 50));
+        this.commandHelp.setBackground(Color.DARK_GRAY);
+        this.clearConsole.setPreferredSize(new Dimension(50, 50));
+        this.clearConsole.setBackground(Color.DARK_GRAY);
+
+        this.clearConsole.addActionListener(e -> this.console.setText(""));
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(this.reloadJDA, BorderLayout.NORTH);
+        panel.add(this.commandHelp, BorderLayout.CENTER);
+        panel.add(this.clearConsole, BorderLayout.SOUTH);
+        this.mainPanel.add(panel, BorderLayout.EAST);
+
+        //open the frame
         this.jFrame.setSize(new Dimension(600, 275));
         this.jFrame.setVisible(true);
-        this.log(LogType.INIT, "finished initialising FRAME");
-
-
     }
 
     public void log(final LogType type, final String message) {
